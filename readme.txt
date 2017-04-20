b3726b77336a10a3479bccb7ef45085a00936ba4
rspublishing <rynaldo.stoltz@automattic.com>
2017-04-20 07:11:55 +0200

    initial release

---
+++ readme.txt
@@ -0,0 +1,35 @@
+=== WC Restore “Purchased Items” Column in Orders Page ===
+
+Contributors: rynald0s
+Tags: purchased items, bought items, column, orders page, orders, missing, woocommerce, woocommerce update, missing column, order info, order summary, purchased column, re-instate, restore, 3.0.0
+Requires at least: 4.5.0
+Tested up to: 4.7.3
+Stable tag: 1.0
+WC requires at least: 3.0
+WC tested up to: 3.0.3
+License: GPLv3 or later License
+URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+This plugin restores the “Purchased Items” column in the orders page. 
+
+
+== Description ==
+
+This plugin restores the “Purchased Items” column in the orders page. 
+
+
+== Installation ==
+
+1. Download the plugin & install it to your `wp-content/plugins` folder (or use the Plugins menu through the WordPress Administration section)
+2. Activate the plugin
+3. Enjoy!
+
+
+== Screenshots ==
+
+1. Restored column
+
+== Changelog ==
+
+= 1.0 = 
+ * Initial Release
---
+++ restore-purchased-items-column.php
@@ -0,0 +1,81 @@
+<?php
+
+/*
+ Plugin Name: WC Restore "Purchased Items" Column in Orders Page
+ Plugin URI: https://profiles.wordpress.org/rynald0s
+ Description: This plugin restores the "Purchased Items" column in the orders page. 
+ Author: Rynaldo Stoltz
+ Author URI: https://github.com/rynaldos
+ Version: 1.0
+ License: GPLv3 or later License
+ URI: http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+if ( ! defined( 'ABSPATH' ) ) { 
+    exit; // Exit if accessed directly
+}
+
+/**
+ * Check if WooCommerce is active
+ **/
+
+add_filter('manage_edit-shop_order_columns', 'wc_custom_purchased_column');
+
+function wc_custom_purchased_column($columns)
+{
+    $new_array = array();
+    foreach ($columns as $key => $title) {
+        if ($key == 'billing_address') {
+
+            $new_array['order_items'] = __('Purchased', 'woocommerce');
+        }
+
+        $new_array[$key] = $title;
+    }
+    return $new_array;
+}
+
+add_action('manage_shop_order_posts_custom_column', 'wc_shop_custom_column', 10, 2);
+
+function wc_shop_custom_column($column)
+{
+    global $post, $woocommerce, $the_order;
+    switch ($column) {
+
+        case 'order_items':
+            $terms = $the_order->get_items();
+
+            echo '<a href="#" class="show_order_items">' . apply_filters( 'woocommerce_admin_order_item_count', sprintf( _n( '%d item', '%d items', $the_order->get_item_count(), 'woocommerce' ), $the_order->get_item_count() ), $the_order ) . '</a>';
+
+                if ( sizeof( $the_order->get_items() ) > 0 ) {
+
+                    echo '<table class="order_items" cellspacing="0">';
+
+                    foreach ( $the_order->get_items() as $item ) {
+                        $product        = apply_filters( 'woocommerce_order_item_product', $item->get_product(), $item );
+                        $item_meta      = new WC_Order_Item_Meta( $item, $product );
+                        $item_meta_html = $item_meta->display( true, true );
+                        ?>
+                        <tr class="<?php echo apply_filters( 'woocommerce_admin_order_item_class', '', $item, $the_order ); ?>">
+                            <td class="qty"><?php echo esc_html( $item->get_quantity() ); ?></td>
+                            <td class="name">
+                                <?php  if ( $product ) : ?>
+                                    <?php echo ( wc_product_sku_enabled() && $product->get_sku() ) ? $product->get_sku() . ' - ' : ''; ?><a href="<?php echo get_edit_post_link( $product->get_id() ); ?>"><?php echo apply_filters( 'woocommerce_order_item_name', $item->get_name(), $item, false ); ?></a>
+                                <?php else : ?>
+                                    <?php echo apply_filters( 'woocommerce_order_item_name', $item->get_name(), $item, false ); ?>
+                                <?php endif; ?>
+                                <?php if ( ! empty( $item_meta_html ) ) : ?>
+                                    <?php echo wc_help_tip( $item_meta_html ); ?>
+                                <?php endif; ?>
+                            </td>
+                        </tr>
+                        <?php
+                    }
+
+                    echo '</table>';
+
+                } else echo '&ndash;';
+            break;
+
+        }
+}
